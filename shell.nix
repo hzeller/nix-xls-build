2ca500ce3d9e7f9841b3bfcc37d222804ffc3464
Henner Zeller <h.zeller@acm.org>
2024-12-30 11:19:25 +0100

    Make llvm toolchain work.
    Thanks to @lromor

--- shell.nix
+++ shell.nix
@@ -1,44 +1,90 @@
 { pkgs ? import <nixpkgs> {} }:
 let
-  osrelease = pkgs.stdenv.mkDerivation {
-    name = "osrelease";
-    buildCommand = ''
-      mkdir -p $out/etc
-      cp $osrelease $out/etc/os-release
-    '';
-    osrelease = pkgs.writeTextFile {
-      name = "os-release";
-      text = ''
-# Information llvm toolchain uses to choose what to download.
-ID=debian
-VERSION_ID="12"
-    '';
-    };
-  };
-in (pkgs.buildFHSEnv {
-  name = "xls-compile-environment";
-  targetPkgs = pkgs: (with pkgs; [
-    osrelease          # fake os-release so that blaze llvm download works
+  bazelOverride = ((pkgs.bazel_7.override {
+    enableNixHacks = false;
+  }).overrideAttrs (
+    previousAttrs: {
+      # Provide our own hacks, latest ones seem to introduce repo issues.
+      patches = previousAttrs.patches ++ [
+        ./nix/bazel.patch
+      ];
+    }
+  ));
 
-    gcc
-    bazel_7 jdk11        # build tool
-    stdenv
-    git cacert         # some recursive workspace dependencies via git.
-
-    # Various libraries that Python links dynamically
-    python39
-    libxcrypt-legacy
-
-    # Various things that llvm links dynamically
-    libz
-    expat
+  # Required for toolchains_llvm
+  bazelRunLibs = with pkgs; [
+    ncurses5
+    stdenv.cc.cc
+    zlib
     zstd
+    libtinfo
     libxml2
-    ncurses     # this provides libtinfo. Currenly not properly linking. Requires LD_LIBRARY_PATH
- 
-    # Development tools
-    clang-tools_17
-    bazel-buildtools
-  ]);
-  extraOutputsToInstall = [ "dev" ];
-}).env
+    expat
+  ];
+
+  toolchains_llvm = pkgs.stdenv.mkDerivation {
+    name = "toolchains_llvm";
+    src = pkgs.fetchFromGitHub {
+      owner = "bazel-contrib";
+      repo = "toolchains_llvm";
+      rev = "ab5557f9c1c1b086af0c9f6806aaae189bf248c0";
+      sha256 = "sha256-HhWqb+WVeR5dR64P4xqv4LSNeLO+AVZqzPNRZcM9A4k=";
+    };
+
+    patches = [
+      (pkgs.substituteAll {
+        src = ./nix/toolchains_llvm.patch;
+        linuxHeadersPath = pkgs.stdenv.cc.libc.dev.linuxHeaders;
+        glibcPath = pkgs.stdenv.cc.libc;
+        glibcDevPath = pkgs.stdenv.cc.libc.dev;
+        stdenvPath = pkgs.stdenv.cc.cc.lib;
+        stdenvDevPath = pkgs.stdenv.cc.cc;
+        extraIncludes = ({ prefix, values }:
+          builtins.concatStringsSep
+            ","
+            (builtins.concatMap (x:
+              [("\"${prefix}\"") ("\""+ x + "\"")]) values)) {
+                prefix = "-idirafter";
+                values = [
+                  "${pkgs.zlib.dev}/include"
+                ];
+              };
+      })
+    ];
+    postPatch = ''
+      # HACK: change to executable so it can be picked up by patchshebangs.
+      chmod 755 ./toolchain/cc_wrapper.sh.tpl
+      patchShebangs .
+    '';
+    installPhase = ''
+      cp -r . $out
+    '';
+  };
+in
+pkgs.mkShell {
+  name = "build-environment";
+  packages = with pkgs; [
+    git
+    bazelOverride
+    jdk17
+    valgrind
+    gopls
+    gdb
+    zlib
+    zlib.dev
+    bc
+  ];
+
+  # Override .bazelversion. We only care to have bazel 7.
+  USE_BAZEL_VERSION = "${bazelOverride.version}";
+  NIX_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath bazelRunLibs;
+  CPATH = pkgs.lib.makeLibraryPath bazelRunLibs;
+  TOOLCHAINS_LLVM = "${toolchains_llvm}";
+  shellHook =
+    ''
+     cat > user.bazelrc <<EOF
+common --override_repository=toolchains_llvm=${toolchains_llvm}
+build:asan --copt -static-libasan
+EOF
+   '';
+}
